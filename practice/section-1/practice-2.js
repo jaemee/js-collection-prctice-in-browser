410db0d76d34c824eaf35679c6e3730153a4c988
bugarja <jae.melanee.bugarin@oocl.com>
2019-09-15 19:55:02 +0800

    remove comments

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -4,10 +4,6 @@ function collectSameElements(collectionA, collectionB) {
   const result = [];
   const arrayB = [];
   var sample = "";
-  // for(var count = 0; count < collectionB.length; count++){
-  //     arrayB.push(collectionB[count][0])
-  // }
-
 
   collectionA.forEach((var1) => 
     collectionB.forEach((arrayB)=>
